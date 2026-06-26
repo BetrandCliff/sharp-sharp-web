f3131f9313bef4de6281dde26734f3905be866bb
BetrandCliff <betrandcliff@gmail.com>
2026-06-26 19:47:26 +0100

    callback modified

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,15 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
+import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
 
   const code = searchParams.get("code");
 
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  const supabase = await createClient();
 
   if (code) {
     await supabase.auth.exchangeCodeForSession(code);
